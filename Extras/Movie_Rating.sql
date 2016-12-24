d52347a657de7ea2401b62a28e8234a725ae069a
Vishal Yadav <vishal2232@users.noreply.github.com>
2016-12-24 19:23:27 +0530

    Update Movie_Rating.sql

--- Extras/Movie_Rating.sql
+++ Extras/Movie_Rating.sql
@@ -9,3 +9,20 @@ Query:
 Select  R.name, M.title, Rat.stars
 from Movie M, Reviewer R, Rating Rat
 where M.mID = Rat.mID and Rat.rID = R.rID and R.name = M.director;
+
+--Q3: Return all reviewer names and movie names together in a single list, alphabetized. (Sorting by the first name of the reviewer and first word in the title is fine; no need for special processing on last names or removing "The".) 
+Query:
+SELECT title FROM Movie
+UNION
+SELECT name FROM Reviewer
+ORDER BY name, title;
+
+--Q4: Find the titles of all movies not reviewed by Chris Jackson. 
+Query:
+SELECT title
+FROM Movie
+WHERE mId NOT IN (
+  SELECT R.mId
+  FROM Rating R, Reviewer Rev
+  WHERE R.rid = Rev.rid and Rev.name = "Chris Jackson"
+);
