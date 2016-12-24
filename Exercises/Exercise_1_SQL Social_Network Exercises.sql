2be2acbfb07ab1438345c21089f457fd262d23a1
Vishal Yadav <vishal2232@users.noreply.github.com>
2016-12-24 19:14:01 +0530

    Add files via upload

---
+++ Exercises/Exercise_1_SQL Social_Network Exercises.sql
@@ -0,0 +1,12 @@
+--Q1: Find the names of all students who are friends with someone named Gabriel. 
+Query:
+select distinct name from Highschooler where ID in (select ID1 from Friend where ID2 in (select ID from Highschooler where name="Gabriel"));
+
+--Q2: For every student who likes someone 2 or more grades younger than themselves, return that student's name and grade, and the name and grade of the student they like. 
+Query:
+select distinct sName, sGrade, lName, lGrade
+from (select h1.name as sName, h1.grade sGrade, h2.name as lName, h2.grade as lGrade, h1.grade-h2.grade as gradeDiff 
+     from Highschooler h1, Likes, Highschooler h2
+     where h1.ID=ID1 and h2.ID=ID2)
+where gradeDiff>1;
+
