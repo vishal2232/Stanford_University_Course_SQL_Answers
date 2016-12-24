ba60b9cd8e19cfbdf77af8aea496b3dff32a8bdc
Vishal Yadav <vishal2232@users.noreply.github.com>
2016-12-24 19:17:18 +0530

    Update Exercise_1_SQL Social_Network Exercises.sql

--- Exercises/Exercise_1_SQL Social_Network Exercises.sql
+++ Exercises/Exercise_1_SQL Social_Network Exercises.sql
@@ -10,3 +10,7 @@ from (select h1.name as sName, h1.grade sGrade, h2.name as lName, h2.grade as lG
      where h1.ID=ID1 and h2.ID=ID2)
 where gradeDiff>1;
 
+--Q3: For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order.
+Query:
+select h1.name, h1.grade, h2.name, h2.grade  from Likes l1, Likes l2, Highschooler h1, Highschooler h2
+where l1.ID1=l2.ID2 and l2.ID1=l1.ID2 and l1.ID1=h1.ID and l1.ID2=h2.ID and h2.name>h1.name;
