d9b0cc1f62bd9591c48a49f781cacbb0ceca1358
Vishal Yadav <vishal2232@users.noreply.github.com>
2016-12-29 23:51:54 +0530

    Update SQL Social-Network Modification Exercises.sql

--- Exercises/SQL Social-Network Modification Exercises.sql
+++ Exercises/SQL Social-Network Modification Exercises.sql
@@ -2,3 +2,9 @@
 Query
 delete from Highschooler
 where grade = 12;
+
+--Q2 If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
+Query
+delete from Likes
+where ID2 in (select ID2 from Friend where Likes.ID1 = ID1) and
+      ID2 not in (select L.ID1 from Likes L where Likes.ID1 = L.ID2);
