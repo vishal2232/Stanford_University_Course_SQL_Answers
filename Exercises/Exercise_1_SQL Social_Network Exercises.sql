cddb9b28db4d5c2826e13679112d4393dc341fe0
Vishal Yadav <vishal2232@users.noreply.github.com>
2017-01-12 23:49:39 +0530

    Update Exercise_1_SQL Social_Network Exercises.sql

--- Exercises/Exercise_1_SQL Social_Network Exercises.sql
+++ Exercises/Exercise_1_SQL Social_Network Exercises.sql
@@ -32,3 +32,13 @@ where ID not in (
   select ID1 from Highschooler H1, Friend, Highschooler H2
   where H1.ID = Friend.ID1 and Friend.ID2 = H2.ID and H1.grade <> H2.grade)
 order by grade, name;
+
+--Q7: For each student A who likes a student B where the two are not friends, find if they have a friend C in common (who can introduce them!). For all such trios, return the name and grade of A, B, and C. 
+Query
+select H1.name, H1.grade, H2.name, H2.grade, H3.name, H3.grade
+from Highschooler H1, Likes, Highschooler H2, Highschooler H3, Friend F1, Friend F2
+where H1.ID = Likes.ID1 and Likes.ID2 = H2.ID and
+  H2.ID not in (select ID2 from Friend where ID1 = H1.ID) and
+  H1.ID = F1.ID1 and F1.ID2 = H3.ID and
+  H3.ID = F2.ID1 and F2.ID2 = H2.ID;
+
