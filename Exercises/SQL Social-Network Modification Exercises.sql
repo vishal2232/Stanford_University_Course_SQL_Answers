0e07a06247534a338d601eedaa03ccd1adf89e17
Vishal Yadav <vishal2232@users.noreply.github.com>
2017-01-16 23:53:32 +0530

    Update SQL Social-Network Modification Exercises.sql

--- Exercises/SQL Social-Network Modification Exercises.sql
+++ Exercises/SQL Social-Network Modification Exercises.sql
@@ -9,3 +9,4 @@ delete from Likes
 where ID2 in (select ID2 from Friend where Likes.ID1 = ID1) and
       ID2 not in (select Lyk.ID1 from Likes Lyk where Likes.ID1 = Lyk.ID2);
 --Q3 For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships with oneself.
+-- my query fails to complete the requirements. if you got it please let me know
